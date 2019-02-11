857ff835c7b0ea758f90816975a8df74331ffe26
Hugo <hugovisantos@gmail.com>
2019-02-11 11:44:38 -0200

    Adicionar arquivos de projeto.

---
+++ Case-Alocacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Case_Alocacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
